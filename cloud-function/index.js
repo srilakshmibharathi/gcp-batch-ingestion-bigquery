7760d01d3c581850f1bd2d883e2966eb33b9b8cc
srilakshmibharathi <41296882+srilakshmibharathi@users.noreply.github.com>
2020-09-14 17:19:56 +0530

    Update index.js

--- cloud-function/index.js
+++ cloud-function/index.js
@@ -32,8 +32,8 @@ exports.goWithTheDataFlow = function(event, callback) {
             parameters: {
               inputFile: `gs://${file.bucket}/${file.name}`
             },
-            jobName: 'called-from-a-cloud-function-batch-pipeline-' + new Date().getTime(),
-            gcsPath: 'gs://batch-pipeline/template/pipeline'
+            jobName: 'called-from-a-cloud-function-pipeline123-' + new Date().getTime(),
+            gcsPath: 'gs://pipeline123/template/pipeline'
           }
         }, function(err, response) {
           if (err) {
